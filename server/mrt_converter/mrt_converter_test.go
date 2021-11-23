18f992099abe8f04e65c87fd114c9018fa0c751d
Yuhao He <yuhaohe@google.com>
2021-11-23 23:45:57 +0000

    Convert an MRT archive of updates into BigQuery-friendly data

--- server/mrt_converter/mrt_converter_test.go
+++ server/mrt_converter/mrt_converter_test.go
@@ -1,7 +1,12 @@
 package converter
 
 import (
+	"bytes"
+	"compress/gzip"
 	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"strings"
 	"testing"
 	"time"
@@ -12,6 +17,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// Fake MRT BGP4MP messages.
 var (
 	// 4-octet ASNs show up as peer and in AS_Path attribute.
 	fakeAS4Ann = mrt.NewBGP4MPMessage(100000, 6447, 0, "1.0.0.0", "2.0.0.0", true, bgp.NewBGPUpdateMessage(nil, []bgp.PathAttributeInterface{
@@ -41,6 +47,9 @@ var (
 		bgp.NewIPAddrPrefix(24, "10.0.0.0"),
 		bgp.NewIPAddrPrefix(24, "20.0.0.0"),
 	}))
+
+	gobgpCmpOpts = cmp.AllowUnexported(bgp.IPAddrPrefix{}, bgp.PrefixDefault{})
+	fakeBzip     = func(r io.Reader) io.Reader { return r }
 )
 
 var (
@@ -61,6 +70,27 @@ var (
 	}
 )
 
+func encodeMRTMessage(t *testing.T, msg *mrt.MRTMessage) []byte {
+	t.Helper()
+	raw, err := msg.Serialize()
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Insert fake microseconds if this is BGP4MP_ET.
+	if msg.Header.Type == mrt.BGP4MP_ET {
+		body := raw[mrt.MRT_COMMON_HEADER_LEN:]
+		msg.Header.Len = uint32(len(body) + 4)
+		header, err := msg.Header.Serialize()
+		if err != nil {
+			t.Fatal(err)
+		}
+		raw = append(header, append([]byte{1, 2, 3, 4}, body...)...)
+	}
+	return raw
+}
+
+// encodeBGP4MP encodes a BGP4MP message, a type of MRT message, into bytes.
+// The common header is not included here.
 func encodeBGP4MP(t *testing.T, msg *mrt.BGP4MPMessage) []byte {
 	t.Helper()
 	raw, err := msg.Serialize()
@@ -85,6 +115,15 @@ func marshalAttr(attr bgp.PathAttributeInterface) string {
 		log.Fatal(err)
 	}
 	return string(raw)
+}
+
+func fakeMRTMessage(t *testing.T, timestamp time.Time, mrtType mrt.MRTType, subType mrt.MRTSubTyper, body mrt.Body) *mrt.MRTMessage {
+	t.Helper()
+	m, err := mrt.NewMRTMessage(uint32(timestamp.Unix()), mrtType, subType, body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return m
 }
 
 func TestParseUpdate(t *testing.T) {
@@ -180,7 +219,7 @@ func TestParseUpdate(t *testing.T) {
 			if gotErr := err != nil; test.wantErr != gotErr {
 				t.Errorf("parseUpdate() = err %v; wantErr = %v", err, test.wantErr)
 			}
-			if diff := cmp.Diff(test.want, got, cmp.AllowUnexported(bgp.PrefixDefault{}, bgp.IPAddrPrefix{})); diff != "" {
+			if diff := cmp.Diff(test.want, got, gobgpCmpOpts); diff != "" {
 				t.Errorf("parseUpdate diff: (-got +want)\n%s", diff)
 			}
 		})
@@ -228,4 +267,205 @@ func TestExtractCollector(t *testing.T) {
 			}
 		})
 	}
+}
+
+func concatMsgs(msgs ...[]byte) []byte {
+	var res []byte
+	for _, msg := range msgs {
+		res = append(res, msg...)
+	}
+	return res
+}
+
+func TestConvertMRT(t *testing.T) {
+	fakeTime := time.Now()
+	unextended := time.Unix(fakeTime.Unix(), 0)
+	tests := []struct {
+		desc      string
+		collector string
+		archive   []byte
+		want      []*update
+		wantErr   bool
+	}{
+		{
+			desc:      "convert an archive with one AS4 update",
+			collector: "route-views2",
+			archive:   encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP, mrt.MESSAGE_AS4, fakeAS4Ann)),
+			want: []*update{{
+				Collector:  "route-views2",
+				SeenAt:     unextended,
+				PeerAS:     100000, // 4-octet ASN as peer.
+				Announced:  []string{"10.0.0.0/24", "20.0.0.0/24"},
+				Attributes: []*attributePayload{fourOctetASPath},
+			}},
+		},
+		{
+			desc:      "convert an archive with non-AS4 update",
+			collector: "route-views3",
+			archive:   encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP, mrt.MESSAGE, fakeAnn)),
+			want: []*update{{
+				Collector:  "route-views3",
+				SeenAt:     unextended,
+				PeerAS:     15169,
+				Announced:  []string{"30.0.0.0/24", "40.0.0.0/24"},
+				Attributes: []*attributePayload{twoOctetAS4Path, twoOctetASPath},
+			}},
+		},
+		{
+			desc:      "convert an archive with withdrawal",
+			collector: "route-views3",
+			archive:   encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP, mrt.MESSAGE_AS4, fakeAS4Withdrawal)),
+			want: []*update{{
+				Collector:  "route-views3",
+				SeenAt:     unextended,
+				PeerAS:     100000,
+				Withdrawn:  []string{"30.0.0.0/24", "40.0.0.0/24"},
+				Attributes: nil,
+			}},
+		},
+		{
+			desc:      "convert an archive with BGP4_ET",
+			collector: "route-views3",
+			archive:   encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP_ET, mrt.MESSAGE_AS4, fakeAS4Withdrawal)),
+			want: []*update{{
+				Collector:  "route-views3",
+				SeenAt:     unextended,
+				PeerAS:     100000,
+				Withdrawn:  []string{"30.0.0.0/24", "40.0.0.0/24"},
+				Attributes: nil,
+			}},
+		}, {
+			desc:      "convert an archive with multiple updates",
+			collector: "route-views3",
+			archive: concatMsgs(
+				encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP, mrt.MESSAGE_AS4, fakeAS4Ann)),
+				encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP, mrt.MESSAGE, fakeAnn)),
+				encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP_ET, mrt.MESSAGE_AS4, fakeAS4Withdrawal)),
+			),
+			want: []*update{{
+				Collector:  "route-views3",
+				SeenAt:     unextended,
+				PeerAS:     100000, // 4-octet ASN as peer.
+				Announced:  []string{"10.0.0.0/24", "20.0.0.0/24"},
+				Attributes: []*attributePayload{fourOctetASPath},
+			}, {
+				Collector:  "route-views3",
+				SeenAt:     unextended,
+				PeerAS:     15169,
+				Announced:  []string{"30.0.0.0/24", "40.0.0.0/24"},
+				Attributes: []*attributePayload{twoOctetAS4Path, twoOctetASPath},
+			}, {
+				Collector:  "route-views3",
+				SeenAt:     unextended,
+				PeerAS:     100000,
+				Withdrawn:  []string{"30.0.0.0/24", "40.0.0.0/24"},
+				Attributes: nil,
+			}},
+		}, {
+			desc:      "incomplete message - bad header",
+			collector: "route-views3",
+			archive: concatMsgs(
+				encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP, mrt.MESSAGE, fakeAnn)),
+				encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP_ET, mrt.MESSAGE_AS4, fakeAS4Withdrawal))[:10],
+			),
+			wantErr: true,
+		}, {
+			desc:      "incomplete message - bad body",
+			collector: "route-views3",
+			archive: concatMsgs(
+				encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP_ET, mrt.MESSAGE_AS4, fakeAS4Withdrawal))[:mrt.MRT_COMMON_HEADER_LEN+1],
+			),
+			wantErr: true,
+		}, {
+			desc:      "parsing failed during converion",
+			collector: "route-views3",
+			// Full MRT message & no nlri, withdrawal or attributes, but one
+			// value inside the BGP payload is wrong.
+			archive: concatMsgs([]byte{97, 157, 202, 61, 0, 16, 0, 4, 0, 0, 0, 43, 0, 1, 134, 160, 0, 0,
+				25, 47, 0, 0, 0, 1, 1, 0, 0, 0, 2, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255,
+				255, 255, 255, 255, 255, 255, 255, 255, 255,
+				0, 23, 2, 100, 0, 0, 0}, // Wrong withdrawn routes length (100).
+				encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP, mrt.MESSAGE_AS4, fakeAS4Withdrawal))),
+
+			want: []*update{{
+				Collector:  "route-views3",
+				SeenAt:     unextended,
+				PeerAS:     100000,
+				Withdrawn:  []string{"30.0.0.0/24", "40.0.0.0/24"},
+				Attributes: nil,
+			}},
+		}, {
+			desc:      "ignore unrecognized types of messages",
+			collector: "route-views3",
+			archive: concatMsgs(
+				encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP, mrt.MESSAGE_AS4, fakeAS4Withdrawal)),
+				encodeMRTMessage(t, fakeMRTMessage(t, fakeTime, mrt.BGP4MP, mrt.STATE_CHANGE,
+					mrt.NewBGP4MPStateChange(15169, 6447, 0, "1.0.0.0", "2.0.0.0", true, mrt.CONNECT, mrt.ACTIVE))),
+			),
+			want: []*update{{
+				Collector:  "route-views3",
+				SeenAt:     unextended,
+				PeerAS:     100000,
+				Withdrawn:  []string{"30.0.0.0/24", "40.0.0.0/24"},
+				Attributes: nil,
+			}},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			// ignoreBzip will disable bzip2 decompression in tests. Golang doesn't have a
+			// bzip2 encoder, and it will be difficult to create test data compressed by
+			// bzip2, so we disable bzip2 in tests.
+			dst := bytes.NewBuffer(nil)
+			err := convert(test.collector, test.archive, dst, fakeBzip)
+			if gotErr := err != nil; test.wantErr != gotErr {
+				t.Errorf("convert() = err %v; wantErr = %v", err, test.wantErr)
+			}
+			if err != nil {
+				t.Log(err)
+				return
+			}
+
+			// Decompress written data.
+			r, err := gzip.NewReader(dst)
+			if err != nil {
+				t.Fatal(err)
+			}
+			got, err := ioutil.ReadAll(r)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			var want []byte
+			for _, u := range test.want {
+				updateJSON, err := json.Marshal(u)
+				if err != nil {
+					t.Fatal(err)
+				}
+				want = append(want, append(updateJSON, '\n')...)
+			}
+			if string(want) != string(got) {
+				t.Errorf("convert() outputs mismatched:\nwant: %s\ngot: %s", string(want), string(got))
+			}
+		})
+	}
+}
+
+type badWriter struct {
+	err error
+}
+
+func (w *badWriter) Write([]byte) (int, error) {
+	return 0, w.err
+}
+
+func TestConvertMRTErrors(t *testing.T) {
+	t.Run("bad writer", func(t *testing.T) {
+		dst := &badWriter{err: fmt.Errorf("GCS not available")}
+		err := convert("routeviews.sg", encodeMRTMessage(t, fakeMRTMessage(t, time.Now(), mrt.BGP4MP, mrt.MESSAGE, fakeAnn)), dst,
+			fakeBzip)
+		if err == nil {
+			t.Error("convert() => nil err; want non-nil err")
+		}
+	})
 }
